05f0697123cc11aefc656b11d85e5cb9057700a6
SpencerSymington <theSpencerSymington@gmail.com>
2018-10-28 22:50:46 -0230

    made it work

--- Calculator/main.m
+++ Calculator/main.m
@@ -7,11 +7,49 @@
 //
 
 #import <Foundation/Foundation.h>
-
+enum Operation {
+    add,
+    subtract,
+    multiply,
+    divide
+};
+NSInteger doMath(NSInteger firstNumber, NSInteger secondNumber, NSInteger operation ){
+    
+    switch(operation){
+            
+        case add:
+            return firstNumber + secondNumber;
+           
+            
+        case subtract:
+            return firstNumber - secondNumber;
+            
+            
+        case multiply:
+            return firstNumber * secondNumber;
+            
+            
+        case divide:
+            //TODO: what about remainder?
+            return firstNumber / secondNumber;
+            
+        default:
+            return firstNumber + secondNumber;
+    }
+    
+   
+}
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        enum Operation currentOperation = subtract;
+        
+        NSInteger firstNumber = 10;
+        NSInteger secondNumber = 3;
+        NSInteger result = 0;
+        
+        result = doMath(firstNumber,secondNumber,currentOperation);
+        
+        NSLog(@"First_Number = %li \nSecond_Number = %li \nResult = %li ",firstNumber,secondNumber,result);
     }
     return 0;
 }
